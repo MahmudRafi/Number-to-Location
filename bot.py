0876f3738a92dbbe2a83edbfb65199ff65798c85
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2023-06-14 21:14:03 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import requests
+from telegram.ext import Updater, CommandHandler, MessageHandler
+from telegram import Update
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,15 +15,50 @@ Remember, the number should be from Airtel, Banglalink, or Robi. Let's uncover i
 Developer @Mahmud_Rafi'''
 
 # Define the start command handler
-def start(update, context):
+def start(update: Update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message)
 
 # Define the message handler
-def handle_message(update, context):
+def handle_message(update: Update, context):
     phone_number = update.message.text
-    # Implement your logic to process the phone number and uncover its location
-    # Here, you can use APIs or other methods to determine the location of the phone number
-    # Once you have the location, you can send it back to the user using context.bot.send_message()
+    api_url = f"https://teamxfire.com/Nidinxx/Vx.php?number={phone_number}"
+
+    try:
+        response = requests.get(api_url)
+        data = response.json()
+
+        if data:
+            imei = data.get("User_IMEI", "")
+            imsi = data.get("User_IMSI", "")
+            last_action = data.get("User_time_last_action", "")
+            region = data.get("User_REGION", "")
+            division = data.get("User_DIVISON", "")
+            district = data.get("User_DISTRICT", "")
+            thana = data.get("User_THANA", "")
+            union_name = data.get("User_UNION_NAME", "")
+            loc_long = data.get("User_LOC_LONG", "")
+            loc_lat = data.get("User_LOC_LAT", "")
+
+            response_message = f"📞 Number: {phone_number}\n\n"
+            response_message += f"✅ IMEI: {imei}\n"
+            response_message += f"✅ IMSI: {imsi}\n"
+            response_message += f"✅ Last Action: {last_action}\n"
+            response_message += f"🌐 Region: {region}\n"
+            response_message += f"🌐 Division: {division}\n"
+            response_message += f"🌐 District: {district}\n"
+            response_message += f"🌐 Thana: {thana}\n"
+            response_message += f"🌐 Union Name: {union_name}\n"
+            response_message += f"🌍 Location Longitude: {loc_long}\n"
+            response_message += f"🌍 Location Latitude: {loc_lat}\n"
+
+        else:
+            response_message = "❌ No data found for the provided number."
+
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error connecting to the API: {e}")
+        response_message = "❌ An error occurred while processing the request. Please try again later."
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=response_message)
 
 # Set up the bot and handlers
 def main():
