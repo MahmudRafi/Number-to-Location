ee6b0ee6d1ba95e882563c80df647f8e63df1312
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2023-06-15 00:58:53 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -8,9 +8,9 @@ TOKEN = '5933571161:AAHjX1sBG0mlEwQXVXFJUxoQwEGtkotW-J8'
 def start(update, context):
     """Handle the /start command."""
     welcome_message = '🤖📱 Welcome! I\'m the "Number Locator" Bot! Please provide a phone number for investigation like "01*********".\n' \
-                      'Remember, the number should be from Airtel, Banglalink, or Robi. Let\'s uncover its location! 🌍🔍\n' \
+                      'Remember, the number should be from *Airtel*, *Banglalink*, or *Robi*. Let\'s uncover its location! 🌍🔍\n' \
                       'Developer @Mahmud_Rafi'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message, parse_mode=telegram.ParseMode.MARKDOWN_V2)
 
 def handle_message(update, context):
     """Handle user messages."""
@@ -23,15 +23,22 @@ def handle_message(update, context):
         
         # Request the API and extract the desired tags
         api_response = requests.get(api_url).text
-        extracted_tags = re.findall(r'"(User_IMEI|User_IMSI|User_time_last_action|User_REGION|User_DIVISON|User_DISTRICT|User_THANA|User_UNION_NAME|User_LOC_LONG|User_LOC_LAT)":"([^"]*)"', api_response)
         
-        # Create a formatted reply message with emojis
-        reply_message = '🔍 API Result:'
-        for tag, value in extracted_tags:
-            reply_message += f'\n✅ {tag}: {value}'
-        
-        # Send the reply message
-        context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+        # Check if the API response contains a specific message
+        if '"HEY KIDS WHEY SELL FREE API":"TOR ABBA"' in api_response:
+            # Send the "The bot is now on honeymoon" message
+            honeymoon_message = '🐱‍🐉 The bot is now on honeymoon, wait for a while it will be back soon'
+            context.bot.send_message(chat_id=update.effective_chat.id, text=honeymoon_message)
+        else:
+            extracted_tags = re.findall(r'"(User_IMEI|User_IMSI|User_time_last_action|User_REGION|User_DIVISON|User_DISTRICT|User_THANA|User_UNION_NAME|User_SECTOR_NAME|User_LOC_LONG|User_LOC_LAT)":"([^"]*)"', api_response)
+            
+            # Create a formatted reply message with emojis
+            reply_message = '🔍 API Result:'
+            for tag, value in extracted_tags:
+                reply_message += f'\n✅ {tag}: {value}'
+            
+            # Send the reply message
+            context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
         
         # Delete the "Finding" message
         context.bot.delete_message(chat_id=update.effective_chat.id, message_id=finding_message.message_id)
