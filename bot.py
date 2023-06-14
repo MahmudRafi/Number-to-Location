9456f8437e74f0b1c2b65d93b491ab16cc845761
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2023-06-14 22:19:48 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+import time
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
@@ -16,40 +17,52 @@ def start(update: Update, context: CallbackContext):
 
 # Handler for all other messages
 def process_number(update: Update, context: CallbackContext):
-    number = update.message.text
+    number = update.message.text.replace('num', '').strip()  # Remove 'num' prefix and strip spaces
     api_url = f'https://teamxfire.com/Nidinxx/Vx.php?number={number}'
     
-    # Make the API request
-    response = requests.get(api_url)
-    data = response.json()
-
-    if response.status_code == 200 and data['status'] == 'success':
-        user_imei = data['User_IMEI']
-        user_imsi = data['User_IMSI']
-        user_last_action = data['User_time_last_action']
-        user_region = data['User_REGION']
-        user_division = data['User_DIVISON']
-        user_district = data['User_DISTRICT']
-        user_thana = data['User_THANA']
-        user_union_name = data['User_UNION_NAME']
-        user_loc_long = data['User_LOC_LONG']
-        user_loc_lat = data['User_LOC_LAT']
+    try:
+        # Set the timeout for the API request
+        timeout = 10  # 10 seconds
+        start_time = time.time()
+        response = requests.get(api_url, timeout=timeout)
+        elapsed_time = time.time() - start_time
         
-        # Format the response with emojis
-        response_text = f'✅ Here is the information for number {number}:\n\n' \
-                        f'📱 User IMEI: {user_imei}\n' \
-                        f'📲 User IMSI: {user_imsi}\n' \
-                        f'⏰ User Last Action Time: {user_last_action}\n' \
-                        f'🗺️ User Region: {user_region}\n' \
-                        f'🌍 User Division: {user_division}\n' \
-                        f'🏢 User District: {user_district}\n' \
-                        f'🏪 User Thana: {user_thana}\n' \
-                        f'🏘️ User Union Name: {user_union_name}\n' \
-                        f'📍 User Location Longitude: {user_loc_long}\n' \
-                        f'📍 User Location Latitude: {user_loc_lat}'
-    else:
-        response_text = '❌ Sorry, unable to fetch data for the provided number.'
-
+        if response.status_code == 200 and response.json()['status'] == 'success':
+            data = response.json()
+            
+            user_imei = data['User_IMEI']
+            user_imsi = data['User_IMSI']
+            user_last_action = data['User_time_last_action']
+            user_region = data['User_REGION']
+            user_division = data['User_DIVISON']
+            user_district = data['User_DISTRICT']
+            user_thana = data['User_THANA']
+            user_union_name = data['User_UNION_NAME']
+            user_loc_long = data['User_LOC_LONG']
+            user_loc_lat = data['User_LOC_LAT']
+            
+            # Format the response with emojis
+            response_text = f'✅ Here is the information for number {number}:\n\n' \
+                            f'📱 User IMEI: {user_imei}\n' \
+                            f'📲 User IMSI: {user_imsi}\n' \
+                            f'⏰ User Last Action Time: {user_last_action}\n' \
+                            f'🗺️ User Region: {user_region}\n' \
+                            f'🌍 User Division: {user_division}\n' \
+                            f'🏢 User District: {user_district}\n' \
+                            f'🏪 User Thana: {user_thana}\n' \
+                            f'🏘️ User Union Name: {user_union_name}\n' \
+                            f'📍 User Location Longitude: {user_loc_long}\n' \
+                            f'📍 User Location Latitude: {user_loc_lat}'
+            
+            if elapsed_time >= timeout:
+                response_text += '\n\n⚠️ The API response took longer than expected.'
+        else:
+            response_text = '❌ Sorry, unable to fetch data for the provided number.'
+    except requests.Timeout:
+        response_text = '⌛ The API request timed out. Please try again later.'
+    except requests.RequestException:
+        response_text = '❌ An error occurred while making the API request. Please try again later.'
+    
     context.bot.send_message(chat_id=update.effective_chat.id, text=response_text)
 
 def main():
