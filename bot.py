e155996318e4f1d0224b4e26229ab315df1d8def
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2023-06-16 17:00:15 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,60 +1,33 @@
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import requests
-import re
 
 TOKEN = '5933571161:AAHjX1sBG0mlEwQXVXFJUxoQwEGtkotW-J8'
 
 def start(update, context):
-    """Handle the /start command."""
-    welcome_message = '🤖📱 Welcome! I\'m the "Number Locator" Bot! Please provide a phone number for investigation like "01*********".\n' \
-                      'Remember, the number should be from *Airtel*, *Banglalink*, or *Robi*. Let\'s uncover its location! 🌍🔍\n' \
-                      'Developer @Mahmud_Rafi'
-    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message, parse_mode=telegram.ParseMode.MARKDOWN_V2)
+    welcome_message = '''🤖📱 Welcome! I'm the "Number Locator" Bot! Please provide a phone number for investigation like "01*********".
+Remember, the number should be from Airtel, Banglalink, or Robi. Let's uncover its location! 🌍🔍
+Developer @Mahmud_Rafi'''
+    context.bot.send_message(chat_id=update.effective_chat.id, text=welcome_message)
 
 def handle_message(update, context):
-    """Handle user messages."""
-    message = update.message.text
-    if message.startswith("01") and len(message) == 11:
-        api_url = f"https://teamtasik.xyz/api/siminfo.php?key=free&phone={message}"
-        
-        # Send the "Finding" message
-        finding_message = context.bot.send_message(chat_id=update.effective_chat.id, text='Finding🧐')
-        
-        # Request the API and extract the desired tags
-        api_response = requests.get(api_url).text
-        
-        # Check if the API response contains a specific message
-        if '"HEY KIDS WHEY SELL FREE API":"TOR ABBA"' in api_response:
-            # Send the "The bot is now on honeymoon" message
-            honeymoon_message = '💆‍♂️🏮 The bot is now having a Thai massage, please wait for a while and will be back soon'
-            context.bot.send_message(chat_id=update.effective_chat.id, text=honeymoon_message)
-        else:
-            extracted_tags = re.findall(r'"(User_IMEI|User_IMSI|User_time_last_action|User_REGION|User_DIVISON|User_DISTRICT|User_THANA|User_UNION_NAME|User_SECTOR_NAME|User_LOC_LONG|User_LOC_LAT)":"([^"]*)"', api_response)
-            
-            # Create a formatted reply message with emojis
-            reply_message = '🔍 Result:'
-            for tag, value in extracted_tags:
-                reply_message += f'\n✅ {tag}: {value}'
-            
-            # Send the reply message
-            context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
-        
-        # Delete the "Finding" message
-        context.bot.delete_message(chat_id=update.effective_chat.id, message_id=finding_message.message_id)
+    phone_number = update.message.text.strip()
+    if phone_number.startswith('01') and len(phone_number) == 11:
+        api_url = f'https://teamtasik.xyz/api/siminfo.php?key=free&phone={phone_number}'
+        response = requests.get(api_url)
+        api_result = response.text
+        context.bot.send_message(chat_id=update.effective_chat.id, text=api_result)
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='⚠️ Invalid phone number format. Please provide a valid number.')
+        error_message = 'Invalid phone number! Please provide a valid Bangladeshi number starting with "01" and consisting of 11 digits.'
+        context.bot.send_message(chat_id=update.effective_chat.id, text=error_message)
 
 def main():
-    """Start the bot."""
     updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+    dp = updater.dispatcher
 
-    # Add handlers
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text, handle_message))
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
 
-    # Start the bot
     updater.start_polling()
     updater.idle()
 
