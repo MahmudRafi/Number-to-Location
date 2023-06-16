d61fbf932f869f5804fe444eff952384d55acf46
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2023-06-16 17:05:54 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import requests
+import json
 
 TOKEN = '5933571161:AAHjX1sBG0mlEwQXVXFJUxoQwEGtkotW-J8'
 
@@ -15,11 +16,82 @@ def handle_message(update, context):
     if phone_number.startswith('01') and len(phone_number) == 11:
         api_url = f'https://teamtasik.xyz/api/siminfo.php?key=free&phone={phone_number}'
         response = requests.get(api_url)
-        api_result = response.text
-        context.bot.send_message(chat_id=update.effective_chat.id, text=api_result)
+        api_result = response.json()
+        formatted_result = format_api_result(api_result)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=formatted_result)
     else:
         error_message = 'Invalid phone number! Please provide a valid Bangladeshi number starting with "01" and consisting of 11 digits.'
         context.bot.send_message(chat_id=update.effective_chat.id, text=error_message)
+
+def format_api_result(api_result):
+    if 'User_IMEI' in api_result:
+        imei = api_result['User_IMEI']
+    else:
+        imei = 'Not available'
+
+    if 'User_IMSI' in api_result:
+        imsi = api_result['User_IMSI']
+    else:
+        imsi = 'Not available'
+
+    if 'User_date_last_action' in api_result:
+        last_action_date = api_result['User_date_last_action']
+    else:
+        last_action_date = 'Not available'
+
+    if 'User_time_last_action' in api_result:
+        last_action_time = api_result['User_time_last_action']
+    else:
+        last_action_time = 'Not available'
+
+    if 'User_SECTOR_NAME' in api_result:
+        sector_name = api_result['User_SECTOR_NAME']
+    else:
+        sector_name = 'Not available'
+
+    if 'User_UNION_NAME' in api_result:
+        union_name = api_result['User_UNION_NAME']
+    else:
+        union_name = 'Not available'
+
+    if 'User_THANA' in api_result:
+        thana = api_result['User_THANA']
+    else:
+        thana = 'Not available'
+
+    if 'User_DISTRICT' in api_result:
+        district = api_result['User_DISTRICT']
+    else:
+        district = 'Not available'
+
+    if 'User_DIVISON' in api_result:
+        division = api_result['User_DIVISON']
+    else:
+        division = 'Not available'
+
+    if 'User_LOC_LONG' in api_result:
+        loc_long = api_result['User_LOC_LONG']
+    else:
+        loc_long = 'Not available'
+
+    if 'User_LOC_LAT' in api_result:
+        loc_lat = api_result['User_LOC_LAT']
+    else:
+        loc_lat = 'Not available'
+
+    formatted_result = f'''📱 User_IMEI: {imei}
+🆔 User_IMSI: {imsi}
+📅 User_last_action_date: {last_action_date}
+🕒 User_last_action_time: {last_action_time}
+📍 User_sector_name: {sector_name}
+🌐 User_union_name: {union_name}
+📍 User_thana: {thana}
+🏢 User_district: {district}
+🌍 User_division: {division}
+🗺️ User_loc_long: {loc_long}
+🗺️ User_loc_lat: {loc_lat}'''
+
+    return formatted_result
 
 def main():
     updater = Updater(TOKEN, use_context=True)
