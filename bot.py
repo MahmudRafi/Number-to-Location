7792cca674339a5020376bbf03ea7f82e0da31f9
Mahmud Rafi <87897429+MahmudRafi@users.noreply.github.com>
2023-06-20 00:23:40 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,20 @@ import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import requests
 import json
+import time
 
 TOKEN = '5933571161:AAHjX1sBG0mlEwQXVXFJUxoQwEGtkotW-J8'
+FREE_REQUEST_LIMIT = 2
+FREE_REQUEST_DURATION = 12 * 60 * 60  # 12 hours in seconds
+
+class User:
+    def __init__(self, chat_id, is_premium=False, last_request_time=0, request_count=0):
+        self.chat_id = chat_id
+        self.is_premium = is_premium
+        self.last_request_time = last_request_time
+        self.request_count = request_count
+
+users = {}
 
 def start(update, context):
     welcome_message = '''🤖📱 Welcome! I'm the "Number Locator" Bot! Please provide a phone number for investigation like "01*********".
@@ -19,7 +31,7 @@ def handle_message(update, context):
     chat_id = str(update.effective_chat.id)
 
     # Fetch the allowed chat IDs from the GitHub repository
-    response = requests.get('http://raw.githubusercontent.com/MahmudRafi/hudaihudai/main/chat_ids.txt')
+    response = requests.get('https://raw.githubusercontent.com/MahmudRafi/hudaihudai/main/chat_ids.txt')
     allowed_chat_ids = response.text.strip().split('\n')
 
     if chat_id not in allowed_chat_ids:
@@ -30,6 +42,30 @@ def handle_message(update, context):
 
     phone_number = update.message.text.strip()
     if phone_number.startswith('01') and len(phone_number) == 11:
+        user = users.get(chat_id)
+        if user:
+            if not user.is_premium and user.request_count >= FREE_REQUEST_LIMIT:
+                # Send a message to get premium subscription
+                message = f"⚠️ You have reached the limit of {FREE_REQUEST_LIMIT} free requests within {FREE_REQUEST_DURATION // 3600} hours. To continue using the service, please contact @Mahmud_Rafi to upgrade to premium subscription. You can request again after {time_left(user.last_request_time)} hours."
+                context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+                return
+
+            # Check the request time limit
+            current_time = time.time()
+            if current_time - user.last_request_time < FREE_REQUEST_DURATION:
+                # Send a message indicating the remaining time
+                message = f"⏳ You have recently made a request. Please wait for {time_left(user.last_request_time)} hours before making another request."
+                context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+                return
+
+            # Update the user's request count and time
+            user.request_count += 1
+            user.last_request_time = current_time
+        else:
+            # Create a new user entry
+            user = User(chat_id, False, time.time(), 1)
+            users[chat_id] = user
+
         api_url = f'https://api.cybersh.xyz/siminfo.php?key=ST&number={phone_number}'
 
         # Send the waiting message
@@ -37,8 +73,8 @@ def handle_message(update, context):
 
         # Fetch the API response
         response = requests.get(api_url)
-        api_result = response.json()["siminfo"]
-        formatted_result = format_api_result(api_result)
+        api_result = response.json()
+        formatted_result = format_api_result(api_result, user.is_premium)
 
         # Delete the waiting message
         context.bot.delete_message(chat_id=update.effective_chat.id, message_id=waiting_message.message_id)
@@ -49,8 +85,7 @@ def handle_message(update, context):
         error_message = 'Invalid phone number! Please provide a valid Bangladeshi number starting with "01" and consisting of 11 digits. Ex. 01000000000'
         context.bot.send_message(chat_id=update.effective_chat.id, text=error_message)
 
-
-def format_api_result(api_result):
+def format_api_result(api_result, is_premium):
     number = api_result["number"]
     imei = api_result["imei"]
     imsi = api_result["imsi"]
@@ -66,7 +101,7 @@ def format_api_result(api_result):
     update = api_result["update"]
     loc = api_result["loc"]
 
-    formatted_result = f'''📱 Number: {number}
+    result_message = f'''📱 Number: {number}
 🆔 IMEI: {imei}
 📇 IMSI: {imsi}
 🌍 Division: {division}
@@ -81,7 +116,15 @@ def format_api_result(api_result):
 ⏰ Last Update: {update}
 🌍 Location: {loc}'''
 
-    return formatted_result
+    if is_premium:
+        result_message = "🌟 Premium User 🌟\n\n" + result_message
+
+    return result_message
+
+def time_left(last_request_time):
+    remaining_time = FREE_REQUEST_DURATION - (time.time() - last_request_time)
+    hours = int(remaining_time // 3600)
+    return hours
 
 def main():
     updater = Updater(TOKEN, use_context=True)
